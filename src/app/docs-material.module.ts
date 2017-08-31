69eb57d9e67d5a3d497332ef1044dac417ed8434
Erik V. Itland <evi@computas.com>
2017-08-31 11:51:19 +0200

    Don't break on latest beta of material.
    When building from scratch I got this error message:
    
    Module '"C:/Users/evi/Documents/Projects/Privat/angular-handsontable/node_modules/@angular/cdk/cdk"' has no exported member 'CdkTableModule'.

--- src/app/docs-material.module.ts
+++ src/app/docs-material.module.ts
@@ -32,7 +32,7 @@ import {
   MdToolbarModule,
   MdTooltipModule,
 } from '@angular/material';
-import {CdkTableModule} from '@angular/cdk';
+import {CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
   exports: [
